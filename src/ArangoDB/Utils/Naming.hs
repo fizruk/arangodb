8132dca55c43d6d5eb3fa021ca27e2ce1a67d8e6
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2018-02-26 02:13:39 +0300

    Change nameModifier to convert to lowerCamelCase

--- src/ArangoDB/Utils/Naming.hs
+++ src/ArangoDB/Utils/Naming.hs
@@ -1,61 +1,79 @@
 module ArangoDB.Utils.Naming where
 
 import Data.Char (toLower, isUpper, isDigit)
-import Data.List (intercalate)
 
 -- | Default name modifier.
--- Transforms to @snake_case@ and cuts longest common whole-word prefix.
+-- Transforms to @lowerCamelCase@ and cuts longest common whole-word prefix.
 --
 -- >>> nameModifier "Dog" "dogName"
 -- "name"
 -- >>> nameModifier "PersonEntry" "personLastName"
--- "last_name"
+-- "lastName"
 -- >>> nameModifier "MessageStatus" "MessageSent"
 -- "sent"
 -- >>> nameModifier "SMSEntry" "smsExpiresAt"
--- "expires_at"
+-- "expiresAt"
 -- >>> nameModifier "Progress" "progress75"
 -- "75"
-nameModifier :: String -> String -> String
-nameModifier prefix name = brokenWord ++ suffix
+nameModifier
+  :: String -- ^ Datatype name.
+  -> String -- ^ Field name (for records) or constructor name (for sum types).
+  -> String
+nameModifier prefix name = concat (lowerFirstWord suffix)
   where
-    (common, _, suffix) = commonPrefix (toSnakeCase prefix ++ "_") (toSnakeCase name)
-    brokenWord = reverse (takeWhile (/= '_') (reverse common))
+    (_, _, suffix) = commonPrefixOn (map toLower) (splitCamelWords prefix) (splitCamelWords name)
+
+    lowerFirstWord [] = []
+    lowerFirstWord (w:ws) = map toLower w : ws
 
 -- | Strip longest common prefix of to lists.
 --
--- >>> commonPrefix "MessageStatus" "MessageDelivered" :: (String, String, String)
+-- >>> commonPrefixOn id "MessageStatus" "MessageDelivered" :: (String, String, String)
 -- ("Message","Status","Delivered")
--- >>> commonPrefix "MessageStatus" "MessageSent" :: (String, String, String)
+-- >>> commonPrefixOn id "MessageStatus" "MessageSent" :: (String, String, String)
 -- ("MessageS","tatus","ent")
 --
--- prop> commonPrefix xs xs == (xs, [], [])
--- prop> commonPrefix (xs ++ ys) (xs ++ zs) == (\(as, bs, cs) -> (xs ++ as, bs, cs)) (commonPrefix ys zs)
--- prop> commonPrefix [] xs == ([], [], xs)
--- prop> commonPrefix xs [] == ([], xs, [])
-commonPrefix :: Eq a => [a] -> [a] -> ([a], [a], [a])
-commonPrefix (x:xs) (y:ys)
-  | x == y    = (x : prefix, xs', ys')
-  | otherwise = ([], x:xs, y:ys)
+-- prop> commonPrefixOn id xs xs == (xs, [], [])
+-- prop> commonPrefixOn id (xs ++ ys) (xs ++ zs) == (\(as, bs, cs) -> (xs ++ as, bs, cs)) (commonPrefix ys zs)
+-- prop> commonPrefixOn id [] xs == ([], [], xs)
+-- prop> commonPrefixOn id xs [] == ([], xs, [])
+commonPrefixOn :: Eq b => (a -> b) -> [a] -> [a] -> ([a], [a], [a])
+commonPrefixOn f (x:xs) (y:ys)
+  | f x == f y = (x : prefix, xs', ys')
+  | otherwise  = ([], x:xs, y:ys)
   where
-    (prefix, xs', ys') = commonPrefix xs ys
-commonPrefix xs ys = ([], xs, ys)
+    (prefix, xs', ys') = commonPrefixOn f xs ys
+commonPrefixOn _ xs ys = ([], xs, ys)
 
--- | Convert @CamelCase@ to @snake_case@.
+-- | Convert @CamelCase@ to @lowerCamelCase@.
 --
--- >>> toSnakeCase "Call"
+-- >>> toLowerCamelCase "Call"
 -- "call"
--- >>> toSnakeCase "CampaignId"
--- "campaign_id"
--- >>> toSnakeCase "SMSId"
--- "sms_id"
--- >>> toSnakeCase "progress75"
--- "progress_75"
-toSnakeCase :: String -> String
-toSnakeCase = map toLower . intercalate "_" . splitCamelWords
+-- >>> toLowerCamelCase "CampaignId"
+-- "campaignId"
+-- >>> toLowerCamelCase "SMSId"
+-- "smsId"
+-- >>> toLowerCamelCase "progress75"
+-- "progress75"
+toLowerCamelCase :: String -> String
+toLowerCamelCase = concat . lowerFirstWord . splitCamelWords
   where
-    splitCamelWords = reverse . splitWordsReversed . reverse
+    lowerFirstWord [] = []
+    lowerFirstWord (w:ws) = map toLower w : ws
 
+-- | Split @CamelCase@ name into its constituent words.
+--
+-- >>> splitCamelWords "CamelCase"
+-- ["Camel", "Case"]
+-- >>> splitCamelWords "SMSEntry"
+-- ["SMS", "Entry"]
+-- >>> splitCamelWords "progress75"
+-- ["progress", "75"]
+--
+-- prop> concat . splitCamelWords == id
+splitCamelWords :: String -> [String]
+splitCamelWords = reverse . splitWordsReversed . reverse
+  where
     splitWordsReversed :: String -> [String]
     splitWordsReversed [] = []
     splitWordsReversed rs
