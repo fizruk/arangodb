12adaee682357015f707d2f0f78e1e3403491261
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2018-02-26 03:17:48 +0300

    Rename Collection to CollectionInfo and add type and isSystem properties

--- src/ArangoDB/Types.hs
+++ src/ArangoDB/Types.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
@@ -11,16 +14,20 @@ import qualified Data.HashMap.Strict as HashMap
 import Data.String (IsString)
 import Data.Text (Text)
 import qualified Data.Text as Text
+import Generics.Deriving.Eq (GEq)
+import Generics.Deriving.Enum (fromEnumDefault, toEnumDefault)
+import GHC.Generics (Generic)
 import Web.HttpApiData (ToHttpApiData)
-import ArangoDB.Utils.Aeson (deriveJSON')
+
+import ArangoDB.Utils.Aeson
 
 -- * Collections
 
 newtype CollectionId = CollectionId Text
-  deriving (IsString, Show, ToJSON, FromJSON)
+  deriving newtype (IsString, Show, ToJSON, FromJSON)
 
 newtype CollectionName = CollectionName Text
-  deriving (IsString, Show, ToHttpApiData, ToJSON, FromJSON)
+  deriving newtype (IsString, Show, ToHttpApiData, ToJSON, FromJSON)
 
 data CollectionStatus
   = CollectionNewBorn
@@ -29,30 +36,75 @@ data CollectionStatus
   | CollectionUnloading
   | CollectionDeleted
   | CollectionLoading
-  deriving (Show, Enum)
+  deriving (Generic, GEq, Show, Bounded)
+
+-- |
+-- >>> map toEnum [1..6] :: [CollectionStatus]
+-- [CollectionNewBorn,CollectionUnloading,CollectionUnloaded,CollectionDeleted,CollectionLoaded,CollectionLoading]
+--
+-- >>> map fromEnum [CollectionNewBorn .. CollectionLoading]
+-- [1,2,3,4,5,6]
+instance Enum CollectionStatus where
+  fromEnum = succ . fromEnumDefault
+  toEnum = toEnumDefault . pred
 
 -- |
 -- >>> decode "[1, 2, 3, 4, 5, 6]" :: Maybe [CollectionStatus]
 -- Just [CollectionNewBorn,CollectionUnloaded,CollectionLoaded,CollectionUnloading,CollectionDeleted,CollectionLoading]
+--
+-- >>> eitherDecode "0" :: Either String CollectionStatus
+-- Left "Error in $: illegal collection status: 0 (expected a value between 1 and 6)"
 instance FromJSON CollectionStatus where
-  parseJSON js = (toEnum . pred) <$> parseJSON js
+  parseJSON = parseJSONBoundedEnum "collection status"
 
 -- |
 -- >>> encode [CollectionNewBorn .. CollectionLoading]
 -- "[1,2,3,4,5,6]"
 instance ToJSON CollectionStatus where
-  toJSON = toJSON . succ . fromEnum
+  toJSON = toJSONEnum
 
-data Collection = Collection
-  { collectionId     :: CollectionId
-  , collectionName   :: CollectionName
-  , collectionStatus :: CollectionStatus
+data CollectionType
+  = DocumentCollection
+  | EdgeCollection
+  deriving (Generic, GEq, Show, Bounded)
+
+-- |
+-- >>> map toEnum [2, 3] :: [CollectionType]
+-- [DocumentCollection,EdgeCollection]
+--
+-- >>> map fromEnum [DocumentCollection, EdgeCollection]
+-- [2,3]
+instance Enum CollectionType where
+  fromEnum = (+2) . fromEnumDefault
+  toEnum   = toEnumDefault . (subtract 2)
+
+-- |
+-- >>> decode "[2, 3]" :: Maybe [CollectionType]
+-- Just [DocumentCollection,EdgeCollection]
+--
+-- >>> eitherDecode "4" :: Either String CollectionType
+-- Left "Error in $: illegal collection type: 4 (expected a value between 2 and 3)"
+instance FromJSON CollectionType where
+  parseJSON = parseJSONBoundedEnum "collection type"
+
+-- |
+-- >>> encode [DocumentCollection, EdgeCollection]
+-- "[2,3]"
+instance ToJSON CollectionType where
+  toJSON = toJSONEnum
+
+data CollectionInfo = CollectionInfo
+  { collectionId       :: CollectionId
+  , collectionName     :: CollectionName
+  , collectionStatus   :: CollectionStatus
+  , collectionType     :: CollectionType
+  , collectionIsSystem :: Bool
   } deriving (Show)
 
 -- * Documents
 
 newtype DocumentId = DocumentId Text
-  deriving (Show, ToJSON, FromJSON)
+  deriving newtype (Show, ToJSON, FromJSON)
 
 mkDocumentId :: CollectionName -> DocumentKey -> DocumentId
 mkDocumentId (CollectionName name) (DocumentKey key)
@@ -64,10 +116,10 @@ splitDocumentId (DocumentId handle) = case Text.splitOn "/" handle of
   _ -> error ("Impossible happened! Invalid document id: " ++ show handle)
 
 newtype DocumentKey = DocumentKey Text
-  deriving (IsString, Show, ToJSON, FromJSON, ToHttpApiData)
+  deriving newtype (IsString, Show, ToJSON, FromJSON, ToHttpApiData)
 
 newtype DocumentRevision = DocumentRevision Text
-  deriving (Show, ToJSON, FromJSON, ToHttpApiData)
+  deriving newtype (Show, ToJSON, FromJSON, ToHttpApiData)
 
 data Document a = Document
   { documentId    :: DocumentId
@@ -95,4 +147,4 @@ instance FromJSON a => FromJSON (Document a) where
     <*> (parseJSON js <|> o .: "value")
 
 -- Template Haskell derivations
-deriveJSON' ''Collection
+deriveJSON' ''CollectionInfo
