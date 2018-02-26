7545546aae04f6ff69814b4d9781a607f884c662
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2018-02-26 03:15:57 +0300

    Add toJSONEnum and parseJSONBoundedEnum helpers

--- src/ArangoDB/Utils/Aeson.hs
+++ src/ArangoDB/Utils/Aeson.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module ArangoDB.Utils.Aeson where
 
 import Data.Aeson
+import Data.Aeson.Types (Parser)
 import Data.Aeson.TH (deriveJSON)
 import ArangoDB.Utils.Naming (nameModifier)
 import Language.Haskell.TH (Name, Q, Dec, nameBase)
@@ -8,7 +10,7 @@ import Language.Haskell.TH (Name, Q, Dec, nameBase)
 -- | Options to derive @'ToJSON'@/@'FromJSON'@ instances.
 --
 -- @aesonOptions prefix@ drops @prefix@ for every field and converts
--- what's left to @snake_case@.
+-- what's left to @lowerCamelCase@.
 aesonOptions :: String -> Options
 aesonOptions prefix = defaultOptions
   { fieldLabelModifier      = nameModifier prefix
@@ -20,3 +22,28 @@ aesonOptions prefix = defaultOptions
 -- | Derive 'ToJSON' and 'FromJSON' with Template Haskell using 'aesonOptions'.
 deriveJSON' :: Name -> Q [Dec]
 deriveJSON' typeName = deriveJSON (aesonOptions (nameBase typeName)) typeName
+
+-- | Convert a value to JSON 'Value' using 'Enum'.
+--
+-- >>> encode (toJSONEnum True)
+-- "1"
+toJSONEnum :: Enum a => a -> Value
+toJSONEnum = toJSON . fromEnum
+
+parseJSONBoundedEnum
+  :: forall a. (Bounded a, Enum a)
+  => String             -- ^ Value description to be printed in error messages.
+  -> Value -> Parser a
+parseJSONBoundedEnum err js = do
+  n <- parseJSON js
+  if minN <= n && n <= maxN
+    then return (toEnum n)
+    else fail (errorMessage n)
+  where
+    minN = fromEnum (minBound :: a)
+    maxN = fromEnum (maxBound :: a)
+
+    errorMessage n = concat
+      [ "illegal " ++ err ++ ": " ++ show n
+      , " (expected a value between " ++ show minN ++ " and " ++ show maxN ++ ")"
+      ]
