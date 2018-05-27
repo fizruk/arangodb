bd9bff9a55a1b9e8c68fb3aeb19e50f4840ab6e5
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2018-05-27 03:12:57 +0300

    Generalise KnownValue to enable Symbol

--- src/ArangoDB/Utils/API.hs
+++ src/ArangoDB/Utils/API.hs
@@ -12,14 +12,20 @@
 module ArangoDB.Utils.API where
 
 import           Data.Proxy
+import           GHC.TypeLits
 import           Servant.API
 import           Servant.Client.Core
 
 data param :==: (value :: k)
 infix 5 :==:
 
-class KnownValue (x :: a) where
-  knownValue :: Proxy x -> a
+type family KnownType k where
+  KnownType Symbol = String
+  KnownType (f a)  = f (KnownType a)
+  KnownType a      = a
+
+class KnownValue (x :: k) where
+  knownValue :: Proxy x -> KnownType k
 
 instance KnownValue 'True  where knownValue _ = True
 instance KnownValue 'False where knownValue _ = False
@@ -28,11 +34,14 @@ instance KnownValue 'Nothing where knownValue _ = Nothing
 instance KnownValue x => KnownValue ('Just x) where
   knownValue _ = Just (knownValue (Proxy @x))
 
+instance KnownSymbol s => KnownValue (s :: Symbol) where
+  knownValue = symbolVal
+
 instance
   ( HasClient m (param :> api)
-  , Client m (param :> api) ~ (a -> Client m api)
+  , Client m (param :> api) ~ (KnownType k -> Client m api)
   , KnownValue value
-  ) => HasClient m ((param :==: (value :: a)) :> api) where
+  ) => HasClient m ((param :==: (value :: k)) :> api) where
 
   type Client m ((param :==: value) :> api) = Client m api
 
